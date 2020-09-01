16e24f4f39a74c55af90c616e9178965400a1c7d
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2020-09-01 16:28:17 +0200

    add scroll to menu

--- animations.js
+++ animations.js
@@ -2,234 +2,242 @@ const tl = new TimelineMax();
 gsap.registerPlugin(ScrollTrigger);
 
 //start
-tl.from(".menu", 0.5, {
+tl.from('.menu', 0.5, {
+	opacity: 0,
+	x: 300
+})
+	.addLabel('start')
+	.from('.first h1', 0.3, {
 		opacity: 0,
-		x: 300,
-	})
-	.addLabel("start")
-	.from(".first h1", 0.3, {
-		opacity: 0,
-		y: -100,
+		y: -100
 	})
 	.from(
-		".first p",
-		0.3, {
+		'.first p',
+		0.3,
+		{
 			opacity: 0,
-			y: 100,
+			y: 100
 		},
-		"start"
+		'start'
 	)
-	.addLabel("button")
-	.from(".first .circle", 0.4, {
+	.addLabel('button')
+	.from('.first .circle', 0.4, {
 		scale: 0,
-		transformOrigin: "50% 50%",
-		stagger: "." + Math.floor(Math.random() * 5),
+		transformOrigin: '50% 50%',
+		stagger: '.' + Math.floor(Math.random() * 5)
 	})
 	.from(
-		".purple",
-		0.4, {
+		'.purple',
+		0.4,
+		{
 			opacity: 0,
-			x: -50,
+			x: -50
 		},
-		"button"
+		'button'
 	)
 	.from(
-		".green",
-		0.4, {
+		'.green',
+		0.4,
+		{
 			opacity: 0,
-			x: 50,
+			x: 50
 		},
-		"button"
+		'button'
 	);
 //team
 let team = gsap.timeline({
 	scrollTrigger: {
-		trigger: ".team",
-		start: "top 80%",
-		end: "+=100px",
-	},
+		trigger: '.team',
+		start: 'top 80%',
+		end: '+=100px'
+	}
 });
 
-team
-	.from(".team #back", 0.5, {
-		scaleX: 0,
-		opacity: 0,
+team.from('.team #back', 0.5, {
+	scaleX: 0,
+	opacity: 0,
+	x: -400,
+	transformOrigin: '0% 50%'
+})
+	.addLabel('team')
+	.from('.team #person1', 0.5, {
 		x: -400,
-		transformOrigin: "0% 50%",
-	})
-	.addLabel("team")
-	.from(".team #person1", 0.5, {
-		x: -400,
-		opacity: 0,
+		opacity: 0
 	})
 	.from(
-		".team #person4",
-		0.5, {
+		'.team #person4',
+		0.5,
+		{
 			x: 400,
-			opacity: 0,
+			opacity: 0
 		},
-		"team"
+		'team'
 	)
 	.from(
-		".team #person2",
-		0.5, {
+		'.team #person2',
+		0.5,
+		{
 			y: -400,
-			opacity: 0,
+			opacity: 0
 		},
-		"team"
+		'team'
 	)
 	.from(
-		".team #person3",
-		0.5, {
+		'.team #person3',
+		0.5,
+		{
 			y: -300,
-			opacity: 0,
+			opacity: 0
 		},
-		"team"
+		'team'
 	);
 
 let about = gsap.timeline({
 	scrollTrigger: {
-		trigger: ".second",
-		start: "top 50%",
-		end: "+=100px",
-	},
+		trigger: '.second',
+		start: 'top 50%',
+		end: '+=100px'
+	}
 });
 
 about
-	.addLabel("header")
-	.from(".header h2", 0.3, {
+	.addLabel('header')
+	.from('.header h2', 0.3, {
 		y: -70,
-		opacity: 0,
+		opacity: 0
 	})
 	.from(
-		".header p",
-		0.3, {
+		'.header p',
+		0.3,
+		{
 			opacity: 0,
-			y: 70,
+			y: 70
 		},
-		"header"
+		'header'
 	)
-	.from(".features .feature", 0.3, {
+	.from('.features .feature', 0.3, {
 		opacity: 0,
 		y: 200,
 		stagger: 0.1,
-		ease: "easeOut",
+		ease: 'easeOut'
 	})
-	.from(".all", 0.2, {
+	.from('.all', 0.2, {
 		scale: 0,
-		transformOrigin: "50% 50%",
+		transformOrigin: '50% 50%'
 	});
 
 //services
 let services = gsap.timeline({
 	scrollTrigger: {
-		trigger: "#services",
-		start: "top 80%",
-		end: "+=100px",
-	},
+		trigger: '#services',
+		start: 'top 80%',
+		end: '+=100px'
+	}
 });
 
 services
-	.from("#services .desc", 0.5, {
+	.from('#services .desc', 0.5, {
 		x: -400,
-		opacity: 0,
+		opacity: 0
 	})
-	.from(".build #window", 0.4, {
+	.from('.build #window', 0.4, {
 		y: -300,
-		opacity: 0,
+		opacity: 0
 	})
-	.addLabel("tabs")
-	.from(".build #tabs g", 0.4, {
+	.addLabel('tabs')
+	.from('.build #tabs g', 0.4, {
 		scale: 0,
-		transformOrigin: "50% 50%",
-		stagger: 0.15,
+		transformOrigin: '50% 50%',
+		stagger: 0.15
 	})
-	.from(".build #squares path", 0.3, {
+	.from('.build #squares path', 0.3, {
 		scale: 0,
-		transformOrigin: "50% 50%",
-		stagger: 0.15,
+		transformOrigin: '50% 50%',
+		stagger: 0.15
 	})
-	.from(".build #person1", 0.5, {
+	.from('.build #person1', 0.5, {
 		opacity: 0,
 		zIndex: -1,
-		x: 300,
+		x: 300
 	})
-	.from(".build #person3", 0.4, {
+	.from('.build #person3', 0.4, {
 		opacity: 0,
-		x: 300,
+		x: 300
 	})
-	.from(".build #person2", 0.4, {
+	.from('.build #person2', 0.4, {
 		y: 300,
-		opacity: 0,
+		opacity: 0
 	});
 
 //socials
 let socials = gsap.timeline({
 	scrollTrigger: {
-		trigger: "#social",
-		start: "top 50%",
-		end: "+=200px",
-	},
+		trigger: '#social',
+		start: 'top 50%',
+		end: '+=200px'
+	}
 });
 
 socials
-	.from("#social .desc", 0.5, {
+	.from('#social .desc', 0.5, {
 		x: -400,
-		opacity: 0,
+		opacity: 0
 	})
-	.addLabel("floor")
-	.from("#box", 0.3, {
+	.addLabel('floor')
+	.from('#box', 0.3, {
 		y: -650,
 		opacity: 0,
-		stagger: 0.15,
+		stagger: 0.15
 	})
 	.from(
-		"#floor",
-		0.4, {
-			transformOrigin: "50% 50%",
-			scale: 0,
+		'#floor',
+		0.4,
+		{
+			transformOrigin: '50% 50%',
+			scale: 0
 		},
-		"floor"
+		'floor'
 	)
-	.from(".delivery #person", 0.4, {
+	.from('.delivery #person', 0.4, {
 		opacity: 0,
-		y: -400,
+		y: -400
 	})
-	.from("#stalks path", 0.25, {
-		transformOrigin: "50% 100%",
+	.from('#stalks path', 0.25, {
+		transformOrigin: '50% 100%',
 		scaleY: 0,
-		stagger: 0.1,
+		stagger: 0.1
 	})
-	.from("#flower", 0.25, {
-		transformOrigin: "50% 100%",
+	.from('#flower', 0.25, {
+		transformOrigin: '50% 100%',
 		scale: 0,
-		stagger: 0.1,
+		stagger: 0.1
 	});
 
 //contact
 
 let contact = gsap.timeline({
 	scrollTrigger: {
-		trigger: "#contact",
-		start: "top 70%",
-	},
+		trigger: '#contact',
+		start: 'top 70%'
+	}
 });
 
 contact
-	.addLabel("header")
-	.from("..contact .title", 0.4, {
+	.addLabel('header')
+	.from('.contact .title', 0.4, {
 		y: -200,
-		opacity: 0,
+		opacity: 0
 	})
 	.from(
-		".contact .subtitle",
-		0.4, {
+		'.contact .subtitle',
+		0.4,
+		{
 			y: 200,
-			opacity: 0,
+			opacity: 0
 		},
-		"header"
+		'header'
 	)
-	.from(".newsletter", 0.4, {
+	.from('.newsletter', 0.4, {
 		x: -200,
-		opacity: 0,
+		opacity: 0
 	});
--- features.js
+++ features.js
@@ -1,25 +1,14 @@
-const all = document.querySelector(".all");
-const features__hide = document.querySelectorAll(".feature__hide");
-all.addEventListener("click", () => {
-    all.style.display = "none";
-    for (const feature of features__hide) {
-        feature.style.display = "flex";
-        tl.from(feature, 0.3, {
-            opacity: 0,
-            y: 200,
-            stagger: 0.1,
-            ease: "easeOut",
-        });
-    }
+const all = document.querySelector('.all');
+const features__hide = document.querySelectorAll('.feature__hide');
+all.addEventListener('click', () => {
+	all.style.display = 'none';
+	for (const feature of features__hide) {
+		feature.style.display = 'flex';
+		tl.from(feature, 0.3, {
+			opacity: 0,
+			y: 200,
+			stagger: 0.1,
+			ease: 'easeOut'
+		});
+	}
 });
-
-function keyboardFocus(e) {
-    if (e.keyCode === 9) { // Tab key
-        document.documentElement.classList.add('keyboard-focus');
-    }
-
-    document.removeEventListener('keydown', keyboardFocus, false);
-}
-
-document.documentElement.classList.remove('no-js');
-document.addEventListener('keydown', keyboardFocus, false);
