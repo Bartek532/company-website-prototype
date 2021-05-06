49994a74f3a04c22b9f88324b0928e969037d083
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-05-06 19:44:01 +0200

    refactorize menu script to ts and optimize it

---
+++ scripts/menu.ts
@@ -0,0 +1,28 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger.js";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const sectionsIdentifiers = ["home", "about", "services", "social", "contact"];
+
+export const menu = () => {
+  sectionsIdentifiers.forEach(identifier => {
+    const activeSection = document.querySelector(`#${identifier}`);
+    const activeLink = document.querySelector(
+      `a[href="#${identifier}"][class^="menu"]`
+    );
+
+    gsap.to(activeLink, {
+      scrollTrigger: {
+        trigger: activeSection,
+        start: "top 50%",
+        end: "bottom center",
+        onToggle: ({ progress, direction, isActive }) => {
+          isActive
+            ? activeLink.classList.add("menu__option--active")
+            : activeLink.classList.remove("menu__option--active");
+        },
+      },
+    });
+  });
+};
